2602047e38bb614306eca7a92ff50abf649b173a
Felipe Eduardo <Felipeeduardol7@outlook.com>
2023-01-23 17:23:38 -0300

    Visual structure

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background: ${props => props.theme["gray-900"]};
+  padding: 4rem 0 12rem;
+`
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 2.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const NewTransactionButton = styled.button`
+  height: 50px;
+
+  border: 0;
+  border-radius: 6px;
+  background: ${props => props.theme["green-500"]};
+  color: ${props => props.theme.white};
+
+  font-weight: bold;
+
+  padding: 0 1.6rem;
+
+  cursor: pointer;
+
+  :hover {
+    background-color: ${props => props.theme["green-700"]};
+    transition: background-color 0.2s;
+  }
+`
